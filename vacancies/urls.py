a191a7826e1073bc9cf04b4eb9da918a10422709
kodoku2010 <dnantonov@stud.etu.ru>
2021-04-28 13:20:54 +0300

    Реализован статичный вывод шаблонов страниц

--- vacancies/urls.py
+++ vacancies/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from .views import MainView
+from .views import MainView, VacanciesView, CategoryView, CompanyView, VacancyView
 
 
 urlpatterns = [
-    path('', MainView.as_view()),
+    path('', MainView.as_view(), name="index"),
+    path('vacancies/', VacanciesView.as_view(), name="vacancies"),
+    path('vacancies/cat/<str:category>/', CategoryView.as_view(), name="category"),
+    path('companies/<int:id>/', CompanyView.as_view(), name="company"),
+    path('vacancies/<int:id>/', VacancyView.as_view(), name="vacancy")
 ]
--- vacancies/views.py
+++ vacancies/views.py
@@ -4,4 +4,24 @@ from django.views import View
 
 class MainView(View):
     def get(self, request):
-        return render(request, 'vacancies/index.html', {})
+        return render(request, 'vacancies/index.html')
+
+
+class VacanciesView(View):
+    def get(self, request):
+        return render(request, 'vacancies/vacancies.html')
+
+
+class CategoryView(View):
+    def get(self, request, category):
+        return render(request, 'vacancies/vacancies.html')
+
+
+class CompanyView(View):
+    def get(self, request, id):
+        return render(request, 'vacancies/company.html')
+
+
+class VacancyView(View):
+    def get(self, request, id):
+        return render(request, 'vacancies/vacancy.html')
