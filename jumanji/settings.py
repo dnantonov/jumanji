74624a2ebdb5f5de9959500d4fcb2a9cd38a1c6f
kodoku2010 <dnantonov@stud.etu.ru>
2021-04-29 20:06:10 +0300

    SECRET KEY вынесен в переменную окружения

--- jumanji/settings.py
+++ jumanji/settings.py
@@ -9,18 +9,17 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!80org!9ekhw%3joghf49p!tkpidt5ljgy5vq)bt_f-m#r%8go'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
