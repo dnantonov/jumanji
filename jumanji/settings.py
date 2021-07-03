b9e2b2234ae7b8acc66e75fa5a75c6bd168872d6
kodoku2010 <dnantonov@stud.etu.ru>
2021-07-03 17:36:10 +0300

    fix tables

--- jumanji/settings.py
+++ jumanji/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = "!80org!9ekhw%3joghf49p!tkpidt5ljgy5vq)bt_f-m#r%8go"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['lit-everglades-74608.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['lit-everglades-74608.herokuapp.com', '0.0.0.0', 'localhost', '127.0.0.1',]
 
 # Application definition
 
